e60d53c9625e414dbca16f725b7799986828eafb
YPYP333YPYP <ypyp333ypyp@gmail.com>
2024-06-10 15:47:27 +0900

    setting: idea init

---
+++ src/main/java/com/example/linkcargo/LinkcargoApplication.java
@@ -0,0 +1,13 @@
+package com.example.linkcargo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkcargoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkcargoApplication.class, args);
+	}
+
+}
