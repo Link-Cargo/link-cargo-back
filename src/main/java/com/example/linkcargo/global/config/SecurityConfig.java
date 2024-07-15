6a72d5800043a12a9abfb7a53c8672310ce07701
donghyuun <bomul0524@gmail.com>
2024-07-16 01:45:02 +0900

    feat: 스프링 시큐리티 설정

---
+++ src/main/java/com/example/linkcargo/global/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.linkcargo.global.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .authorizeHttpRequests(auth ->
+                auth.anyRequest().permitAll()
+            ).csrf((csrfConfig) ->
+                csrfConfig.disable()
+            );
+
+        return http.build();
+    }
+}
