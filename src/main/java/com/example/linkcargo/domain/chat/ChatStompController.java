5edba6e82cc5b89ee19d491565e3a63ef757c0c0
donghyuun <bomul0524@gmail.com>
2024-08-15 15:48:50 +0900

    Feat: 테스트 파일 삭제

--- src/main/java/com/example/linkcargo/domain/chat/ChatStompController.java
+++ src/main/java/com/example/linkcargo/domain/chat/ChatStompController.java
@@ -2,12 +2,11 @@ package com.example.linkcargo.domain.chat;
 
 import com.example.linkcargo.domain.chat.Entity.Chat;
 import com.example.linkcargo.domain.chat.Entity.ChatRoom;
+import com.example.linkcargo.domain.chat.dto.request.ChatRequest.MessageType;
 import com.example.linkcargo.domain.chat.dto.response.ChatEnterResponse;
 import com.example.linkcargo.domain.chat.dto.request.ChatRequest;
 import com.example.linkcargo.domain.chat.dto.response.ChatContentResponse;
 import com.example.linkcargo.domain.user.UserService;
-import java.util.Date;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
@@ -40,7 +39,7 @@ public class ChatStompController {
         Long userId = (Long) accessor.getSessionAttributes().get("userId");
         String sessionId = accessor.getSessionId();
 
-        // 채팅방 입장 메시지를 보낸 경우 (프론트엔드가 먼저 "/user/queue/reply" 를 구독한 상태)
+        // 채팅방 "입장" 메시지를 보낸 경우 (프론트엔드가 먼저 "/user/queue/reply" 를 구독한 상태)
         if (chatRequest.messageType().equals(ChatRequest.MessageType.ENTER)) {
             log.info("ENTER message, chatRequest: {}", chatRequest);
             ChatEnterResponse chatEnterResponse = handleEnterMessage(chatRequest, userId);
@@ -48,32 +47,69 @@ public class ChatStompController {
             log.info("Message sent to /user/queue/reply for session: {}", sessionId);
         }
 
-        // 채팅 전송 메시지를 보낸 경우 (프론트엔드가 먼저 "/sub/chatroom/{id}" 를 구독한 상태)
+        // 채팅 "일반" 전송 메시지를 보낸 경우 (프론트엔드가 먼저 "/sub/chatroom/{id}" 를 구독한 상태)
         if (chatRequest.messageType().equals(ChatRequest.MessageType.CHAT)) {
             log.info("CHAT message, chatRequest: {}", chatRequest);
             ChatContentResponse chatContentResponseResponse = handleChatMessage(chatRequest, chatRoomId, userId);
             messagingTemplate.convertAndSend("/sub/chatroom/" + chatRoomId,
                 chatContentResponseResponse);
         }
+
+        // 채팅 "파일" 전송 메시지를 보낸 경우 (프론트엔드가 먼저 "/sub/chatroom/{id}" 를 구독한 상태)
+        if (chatRequest.messageType().equals(ChatRequest.MessageType.FILE)) {
+            log.info("FILE message, chatRequest: {}", chatRequest);
+            ChatContentResponse chatContentResponse = handleFileMessage(chatRequest, chatRoomId, userId);
+            messagingTemplate.convertAndSend("/sub/chatroom/" + chatRoomId,
+                chatContentResponse);
+        }
+
+
     }
 
+    /**
+     * ENTER 타입 메시지 전송 시
+     */
     private ChatEnterResponse handleEnterMessage(ChatRequest chatRequest, Long userId) {
         Long targetUserId = chatRequest.targetUserId();
         ChatRoom chatRoom = chatService.createOrGetChatRoom(userId, targetUserId);
 
-        return new ChatEnterResponse(chatRoom.getId());
+        return new ChatEnterResponse(MessageType.ENTER, chatRoom.getId());
     }
 
+    /**
+     * CHAT 타입 메시지 전송 시
+     */
     private ChatContentResponse handleChatMessage(ChatRequest chatRequest, Long chatRoomId, Long userId) {
         Chat chat = Chat.builder()
             .chatRoom(chatService.getChatRoom(chatRoomId))
             .sender(userService.getUser(userId))
+            .messageType(MessageType.CHAT)
             .content(chatRequest.content())
+            .fileName("")
+            .fileUrl("")
             .build();
         Chat savedChat = chatService.saveChat(chat);
 
         // 채팅방 ID, 작성자 ID, 내용
-        return new ChatContentResponse(chatRoomId, userId, chatRequest.content(), savedChat.getCreatedAt());
+        return new ChatContentResponse(chatRoomId, userId, MessageType.CHAT, chatRequest.content(), "", "", savedChat.getCreatedAt());
+    }
+
+    /**
+     * FILE 타입 메시지 전송 시
+     */
+    private ChatContentResponse handleFileMessage(ChatRequest chatRequest, Long chatRoomId, Long userId) {
+        Chat chat = Chat.builder()
+            .chatRoom(chatService.getChatRoom(chatRoomId))
+            .sender(userService.getUser(userId))
+            .messageType(MessageType.FILE)
+            .content("")
+            .fileName(chatRequest.fileName())
+            .fileUrl(chatRequest.fileUrl())
+            .build();
+        Chat savedChat = chatService.saveChat(chat);
+
+        // 채팅방 ID, 작성자 ID, 내용
+        return new ChatContentResponse(chatRoomId, userId, MessageType.ENTER, "", chatRequest.fileName(), chatRequest.fileUrl(), savedChat.getCreatedAt());
     }
 
     private MessageHeaders createHeaders(String sessionId) {
--- src/main/java/com/example/linkcargo/domain/chat/Entity/Chat.java
+++ src/main/java/com/example/linkcargo/domain/chat/Entity/Chat.java
@@ -1,5 +1,6 @@
 package com.example.linkcargo.domain.chat.Entity;
 
+import com.example.linkcargo.domain.chat.dto.request.ChatRequest.MessageType;
 import com.example.linkcargo.domain.user.User;
 import com.example.linkcargo.global.entity.JpaBaseEntity;
 import jakarta.persistence.Column;
@@ -11,6 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -38,7 +40,14 @@ public class Chat extends JpaBaseEntity {
     @JoinColumn(name = "sender_id", nullable = false) // 작성자
     private User sender;
 
+    @NotBlank
+    private MessageType messageType;
+
     @Column(nullable = false)
     private String content;
+
+    private String fileName;
+
+    private String fileUrl;
 
 }
