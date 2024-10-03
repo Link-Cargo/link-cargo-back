fc43f18300630ec7f86b78d63dea7ca00c225e97
donghyuun <bomul0524@gmail.com>
2024-10-03 16:11:54 +0900

    feat: email html modified

--- src/test/java/com/example/linkcargo/EmailTest.java
+++ src/test/java/com/example/linkcargo/EmailTest.java
@@ -13,31 +13,30 @@ public class EmailTest {
     private EmailService emailService;
 
 
-//    @Test
-//    @Description("EMAIL SEND TEST")
-//    public void emailSendTest() {
-//        String content = """
-//            (광고) 김동현 화주님! 올해 수출계획은 어떻게 되시나요?
-//            작년 8월에 베트남으로 화장품을 수출하셨네요! 올해 3분기 최저 운임지수를 확인해보세요.
-//
-//            베트남 북부지역 주요항만 3분기 최저 운임 시기 예상 시기 - 9월 초
-//            현재 운임 : 146.13  → 9월 2주차 예상운임 : 120.13
-//
-//            2023.08.21 수출계약 포워딩 업체 정보
-//            포워딩 업체: 금영글로벌(주)
-//            담당자: 홍길동 대리
-//            담당자 전화번호: 010-1234-5678
-//            담당자 이메일: akjfalfj@guem.ac.kr
-//            """;
-//        String contentWithLineBreaks = content.replace("\n", "<br>");
-//
-//        emailService.sendMailNotice(
-//                "hyunnn0524@naver.com",
-//                "(광고) 김동현 화주님! 올해 수출계획은 어떻게 되시나요?",
-//                contentWithLineBreaks,
-//                "화물 정보 입력하러 가기",
-//                "http://www.link-cargo.com"
-//        );
-//    }
+    @Test
+    @Description("EMAIL SEND TEST")
+    public void emailSendTest() {
+        String content = """
+            작년 8월에 베트남으로 화장품을 수출하셨네요! 올해 3분기 최저 운임지수를 확인해보세요.
+
+            베트남 북부지역 주요항만 3분기 최저 운임 시기 예상 시기 - 9월 초
+            현재 운임 : 146.13  → 9월 2주차 예상운임 : 120.13
+
+            2023.08.21 수출계약 포워딩 업체 정보
+            포워딩 업체: 금영글로벌(주)
+            담당자: 홍길동 대리
+            담당자 전화번호: 010-1234-5678
+            담당자 이메일: akjfalfj@guem.ac.kr
+            """;
+        String contentWithLineBreaks = content.replace("\n", "<br>");
+
+        emailService.sendMailNotice(
+                "hyunnn0524@naver.com",
+                "(광고) 김동현 화주님! 올해 수출계획은 어떻게 되시나요?",
+                contentWithLineBreaks,
+                "화물 정보 입력하러 가기",
+                "http://www.link-cargo.com/freight-quote"
+        );
+    }
 
 }
